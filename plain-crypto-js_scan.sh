bfb2949ae318c02326ea4a1e4084aa9f758459c0
Aaron Wurthmann <aaron_wurthmann@hotmail.com>
2026-04-01 09:57:47 -0700

    Update plain-crypto-js_scan.sh
    Attempt #2 to fix colors in SSH

--- plain-crypto-js_scan.sh
+++ plain-crypto-js_scan.sh
@@ -86,17 +86,21 @@ FORCE_COLOR="${FORCE_COLOR:-0}"
 NO_COLOR="${NO_COLOR:-0}"
 
 if { [ -t 1 ] && [ "$NO_COLOR" != "1" ]; } || [ "$FORCE_COLOR" = "1" ]; then
-  C_RESET='\033[0m'
-  C_BOLD='\033[1m'
-  C_RED='\033[31m'
-  C_GREEN='\033[32m'
-  C_YELLOW='\033[33m'
-  C_BLUE='\033[34m'
-  C_MAGENTA='\033[35m'
-  C_CYAN='\033[36m'
+  C_RESET=$'\033[0m'
+  C_BOLD=$'\033[1m'
+  C_DIM=$'\033[2m'
+
+  C_RED=$'\033[31m'
+  C_GREEN=$'\033[32m'
+  C_YELLOW=$'\033[33m'
+  C_BLUE=$'\033[34m'
+  C_MAGENTA=$'\033[35m'
+  C_CYAN=$'\033[36m'
 else
   C_RESET=''
   C_BOLD=''
+  C_DIM=''
+
   C_RED=''
   C_GREEN=''
   C_YELLOW=''
